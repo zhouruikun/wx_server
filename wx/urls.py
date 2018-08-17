7ba4573220260d0d2603154a46b243d2b5c927e0
zhouruikun <915684080@qq.com>
2018-08-17 23:00:02 +0800

    1
    1

--- wx/urls.py
+++ wx/urls.py
@@ -1,8 +1,15 @@
+from django.conf.urls import url
+
 from django.urls import path
 
+from django.views import static
+from django.conf import settings
 from . import views
 
 app_name = 'wx'
 urlpatterns = [
     path('', views.index, name='index'),
+# 增加以下一行，以识别静态资源
+    url(r'^static/(?P<path>.*)$', static.serve,
+        {'document_root': settings.STATIC_ROOT}, name='static')
 ]
