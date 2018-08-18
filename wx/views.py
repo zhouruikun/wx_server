1f5eeeacb37ce9b4a65afb73db72142e1a336c16
zhouruikun <915684080@qq.com>
2018-08-18 10:02:23 +0800

    token验证

--- wx/views.py
+++ wx/views.py
@@ -9,24 +9,23 @@ from django.views import generic
 def index(request):
 
     try:
-        if len(request.POST) == 0:
+        if len(request.GET) == 0:
             return HttpResponse("hello, this is handle view")
-        signature = request.POST['signature']
-        timestamp = request.POST['timestamp']
-        nonce = request.POST['nonce']
-        echostr = request.POST['echostr']
+        signature = request.GET['signature']
+        timestamp = request.GET['timestamp']
+        nonce = request.GET['nonce']
+        echostr = request.GET['echostr']
         token = "zhoukuniyc"  # 请按照公众平台官网\基本配置中信息填写
 
         list = [token, timestamp, nonce]
         list.sort()
-        sha1 = hashlib.sha1()
-        map(sha1.update, list)
-        hashcode = sha1.hexdigest()
+        list = ''.join(list)
+        hashcode = hashlib.sha1(list.encode()).hexdigest()
         print("handle/GET func: hashcode, signature: ", hashcode, signature)
         if hashcode == signature:
             return HttpResponse(echostr)
         else:
-            return HttpResponse("")
+            return HttpResponse('check err')
     except Exception as Argument:
         return HttpResponse(Argument)
 
